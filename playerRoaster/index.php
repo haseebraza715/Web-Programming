217a20426f68637d4d250f8f18b69b3c551cdf61
Haseeb Raza <haseeb.javed715@gmail.com>
2024-11-27 10:54:01 +0100

    complete the modification in index.php

--- playerRoaster/index.php
+++ playerRoaster/index.php
@@ -67,6 +67,51 @@
                 </div>
             </div>
         </div>
+
+        <?php
+            $players = json_decode(file_get_contents('players.json'), true);
+            $badge = array('badge-primary', 'badge-outline');
+            
+            
+            $img = $players[$i]['img'];
+            $goals2024 = $players[$i]['goals2024'];
+            $goals2023 = $players[$i]['goals2023'];
+
+
+                echo
+                '<div class="card card-side bg-base-300 shadow-xl">
+                    <figure class="h-full"><img src="./img/'.$img.'.jpg" class="h-full w-48 object-cover" />
+                    </figure>
+                    <div class="card-body mx-auto text-center w-full p-3 my-auto">
+                        <h2 class="card-title text-center block">'.$name.'</h2>
+                        <div class="card-actions mx-auto text-center block">';
+                        for ($j = 0; $j < count($players[$i]['positions']); $j++){
+                            $position = $players[$i]['positions'][$j];
+                            if ($position == $players[$i]['positions'][0]){
+                                echo '<div class="badge badge-primary">'.$position.'</div>';
+                            }
+                            else{
+                                echo '<div class="badge badge-outline">'.$position.'</div>';
+                            }
+                        }
+                        echo '<div class="stat">
+                                <div class="stat-title">Goals this season</div>
+                                <div class="stat-value">'.$goals2024.'</div>';
+                                if ($goals2024 > $goals2023){
+                                    $percentage = round((($goals2024 - $goals2023) / $goals2023) * 100);
+                                    echo '<div class="stat-desc">'.$percentage.'% more than last season</div>';
+                                }
+                                else if ($goals2024 < $goals2023){
+                                    echo '<div class="stat-desc"> less than last season</div>';
+                                }
+                                else{
+                                    echo '<div class="stat-desc">Same as last season</div>';
+                                }
+                            echo '</div>
+                        </div>
+                    </div>
+                </div>';
+        ?>
     </div>
 </body>
 
